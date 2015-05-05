13a5eb540150eabaa1f2a28c3ae94d19b0b650dc
Josiah Worcester <josiahw@gmail.com>
2015-05-05 17:05:54 -0500

    Do not falsely claim to support ranges on auto-generated data.

--- main.c
+++ main.c
@@ -694,10 +694,12 @@ void *thread(void *fd_p)
 				if(dprintf(c, "Content-Type: %s\r\n", mime) < 0)
 					syslog(LOG_ERR, "dprintf: %m");
 			if(S_ISDIR(buf.st_mode) && strcmp(http, "HTTP/1.1") == 0) {
-				if(dprintf(c, "Transfer-Encoding: chunked\r\n") < 0)
+				if(dprintf(c, "Accept-Ranges: none\r\n"
+					      "Transfer-Encoding: chunked\r\n") < 0)
 					syslog(LOG_ERR, "dprintf: %m");
 			} else {
-				if(dprintf(c, "Content-Length: %jd\r\n", (intmax_t)num_bytes) < 0)
+				if(dprintf(c, "Accept-Ranges: bytes\r\n"
+					      "Content-Length: %jd\r\n", (intmax_t)num_bytes) < 0)
 					syslog(LOG_ERR, "dprintf: %m");
 			}
 			if(start_off > 0 || num_bytes < buf.st_size) {
@@ -709,7 +711,6 @@ void *thread(void *fd_p)
 				lseek(f, start_off, SEEK_SET);
 			}
 			if(dprintf(c, "Date: %s\r\n"
-				      "Accept-Ranges: bytes\r\n"
 				      "Last-Modified: %s\r\n"
 				      "Expires: %s\r\n\r\n",
 				      date, mtime, expires) < 0)
